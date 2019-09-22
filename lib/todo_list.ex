b326dfb314b229131e220aa989f881ce28c687fe
johninvictus <invictusnitude@gmail.com>
2019-09-22 14:20:09 +0300

    initial commit

---
+++ lib/todo_list.ex
@@ -0,0 +1,18 @@
+defmodule TodoList do
+  @moduledoc """
+  Documentation for TodoList.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TodoList.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
